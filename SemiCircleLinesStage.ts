65edf93380c14c5c8529eb39fb85b38dac584307
Anwesh43 <anweshthecool0@gmail.com>
2019-03-26 21:04:17 +0530

    adding constants

---
+++ SemiCircleLinesStage.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const sizeFactor : number = 2.9
+const strokeFactor : number = 90
+const foreColor : string = "#673AB7"
+const backColor : string = "#bdbdbd"
+const nodes : number = 5
+const circles : number = 10
