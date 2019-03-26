8ff9a9efb47edd646ab7b0b91d2c1e5f5ff4acd7
Anwesh43 <anweshthecool0@gmail.com>
2019-03-26 21:33:55 +0530

    adding scale methods

--- SemiCircleLinesStage.ts
+++ SemiCircleLinesStage.ts
@@ -8,3 +8,17 @@ const foreColor : string = "#673AB7"
 const backColor : string = "#bdbdbd"
 const nodes : number = 5
 const circles : number = 10
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    const mxsc : number = maxScale(scale, i, n)
+    return Math.min(1 / n, mxsc)
+}
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a , b) * dir * scGap
+}
