b55c8817761d1e1bc9785f450dafd2ec2bcc5a23
Anwesh43 <anweshthecool0@gmail.com>
2019-03-26 22:26:58 +0530

    adding SCLNode class

--- SemiCircleLinesStage.ts
+++ SemiCircleLinesStage.ts
@@ -132,3 +132,48 @@ class Animator {
         }
     }
 }
+
+class SCLNode {
+
+    prev : SCLNode
+    next : SCLNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new SCLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawSCLNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SCLNode {
+        var curr : SCLNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
